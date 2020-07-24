7bb9c02bf45f820430b2e26f2dfb6a97b494149d
Mikhail <57292068+Degtyarev111@users.noreply.github.com>
2020-07-24 12:39:02 +0300

    Add files via upload

---
+++ OpenFile/OpenFile/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+namespace OpenFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(@"C:\\Users\\degty\\Desktop\\progs\\Calc\\Calc\\bin\\Debug\\netcoreapp3.1\\log.txt")
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+        }
+    }
+}
